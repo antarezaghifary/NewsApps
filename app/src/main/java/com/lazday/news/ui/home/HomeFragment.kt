86553763ab69c75cd4ecb757b2adad60fc4b59a4
antarezaghifary <oneakun08@gmail.com>
2021-07-13 20:13:38 +0700

    Set Adapter Category And Show Data in Adapter 13/7/21

--- app/src/main/java/com/lazday/news/ui/home/HomeFragment.kt
+++ app/src/main/java/com/lazday/news/ui/home/HomeFragment.kt
@@ -7,8 +7,11 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.lazday.news.databinding.CustomToolbarBinding
 import com.lazday.news.databinding.FragmentHomeBinding
+import com.lazday.news.source.news.CategoryModel
+import com.lazday.news.ui.news.CategoryAdapter
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import org.koin.dsl.module
+import timber.log.Timber
 
 val homeModule = module {
     factory { HomeFragment() }
@@ -34,5 +37,16 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         bindingToolbar.textTitle.text = viewModel.title
+        //Timber.e(viewModel.categories.toString())
+        binding.listCategory.adapter = categoryAdapter
+    }
+
+    private val categoryAdapter by lazy {
+        CategoryAdapter(viewModel.categories, object : CategoryAdapter.OnAdapterListener {
+            override fun onClick(category: CategoryModel) {
+                Timber.e(category.id)
+            }
+
+        })
     }
 }
--- app/src/main/java/com/lazday/news/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/lazday/news/ui/home/HomeViewModel.kt
@@ -1,6 +1,7 @@
 package com.lazday.news.ui.home
 
 import androidx.lifecycle.ViewModel
+import com.lazday.news.source.news.CategoryModel
 import com.lazday.news.source.news.NewsRepository
 import org.koin.dsl.module
 
@@ -13,4 +14,16 @@ class HomeViewModel(
         val repository: NewsRepository
 ) : ViewModel() {
     val title = "Berita"
+
+    val categories = listOf<CategoryModel>(
+            CategoryModel("", "Berita Utama"),
+            CategoryModel("business", "Bisnis"),
+            CategoryModel("entertainment", "Hiburan"),
+            CategoryModel("general", "Umum"),
+            CategoryModel("health", "Kesehatan"),
+            CategoryModel("science", "Sains"),
+            CategoryModel("esports", "Olah Raga"),
+            CategoryModel("technology", "Teknologi"),
+
+            )
 }
